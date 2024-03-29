73f45532841a5408b546a81fb7a8834c49270cde
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-03-29 18:00:59 +0900

    solved

---
+++ Silver5/1094.py
@@ -0,0 +1,14 @@
+X = int(input())
+
+sticks = [64, 32, 16, 8, 4, 2, 1]
+count = 0
+
+for stick in sticks:
+    if X >= stick:
+        X -= stick
+        count += 1
+
+    if X == 0:
+        break
+
+print(count)
