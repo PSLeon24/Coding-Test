0a7bb82d1530fade64502e8af4efec8a37ec7568
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-04-08 10:07:14 +0900

    solved

--- Silver4/1018.py
+++ Silver4/1018.py
@@ -1,21 +1,48 @@
+import sys
+
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
-W = []  # White
-B = []  # Black
-len_W, len_B = 0, 0
+# Good chess board
+board_1 = [
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+]
 
-status = []
+board_2 = [
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+    ["B", "W", "B", "W", "B", "W", "B", "W"],
+    ["W", "B", "W", "B", "W", "B", "W", "B"],
+]
+
+board = []
 for _ in range(n):
-    status.append(list(str(input())))
+    board.append(list(map(str, input().rstrip())))
 
-for i in range(n):
-    for j in range(m):
-        if status[i][j] == "W":
-            W.append(status[i][j])
-        elif status[i][j] == "B":
-            B.append(status[i][j])
+results = []
 
-len_W = len(W)
-len_B = len(B)
+for i in range(n - 7):
+    for j in range(m - 7):
+        result_1 = 0
+        result_2 = 0
+        temp = [row[j : j + 8] for row in board[i : i + 8]]
+        for k in range(8):
+            for h in range(8):
+                if temp[k][h] == board_1[k][h]:
+                    result_1 += 1
+                if temp[k][h] == board_2[k][h]:
+                    result_2 += 1
+        results.append(min(result_1, result_2))
 
-print(len_W, len_B)
+print(min(results))
