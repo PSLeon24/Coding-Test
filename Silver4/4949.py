d88d281a24dd27f29169d77bd9623914136bb1f8
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-05-02 13:58:57 +0900

    solved

--- Silver4/4949.py
+++ Silver4/4949.py
@@ -1,35 +1,32 @@
-sentence = []
+import sys
+
+input = sys.stdin.readline
+
 stack = []
 
 while True:
     string = input().rstrip()
-    sentence.append(string)
     if string == ".":
         break
 
-for i in sentence:
-    for j in i:
-        if j == "(" or j == "[":
-            stack.append(j)
-        elif j == ")":
+    for i in string:
+        if i == "(" or i == "[":
+            stack.append(i)
+        elif i == ")":
             if not stack or stack[-1] == "[":
                 print("no")
                 break
             elif stack[-1] == "(":
                 stack.pop()
-        elif j == "]":
+        elif i == "]":
             if not stack or stack[-1] == "(":
                 print("no")
                 break
             elif stack[-1] == "[":
                 stack.pop()
-        elif j == ".":
-            if not stack:
-                print("yes")
-            else:
-                print("no")
     else:
         if not stack:
             print("yes")
         else:
             print("no")
+    stack.clear()
